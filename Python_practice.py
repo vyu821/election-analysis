9fd7885cdb2f51d91e076bc42cc972e130aed404
vyu821 <victor.yu.821@gmail.com>
2020-07-22 19:07:51 -0700

    deleted python_practice.py file

--- Python_practice.py
+++
@@ -1,6 +0,0 @@
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is ", now)

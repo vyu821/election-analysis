088bf6f53935d353e14f9e4a4a6b1a5b60b8d8d0
vyu821 <victor.yu.821@gmail.com>
2020-07-22 19:20:11 -0700

    completed module 3.5.1

--- PyPoll.py
+++ PyPoll.py
@@ -12,10 +12,18 @@ file_to_load = os.path.join("resources","election_results.csv")
 #assign variable to save a file
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+total_votes = 0
+
 #opens election results and reads file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
+    
+    #reads headers and removes row from file_reader
     headers = next(file_reader)
-    print(headers)
+    
+    #counts each row
+    for row in file_reader:
+        total_votes +=1
 
+print(total_votes)
 
